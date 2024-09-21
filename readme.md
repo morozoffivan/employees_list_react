a3de260f9432a611ddba5e191ab3d2c45d8c5996
Morozov Ivan <morozoff.one@yandex.ru>
2024-09-21 22:09:11 +0300

    added props at the list-item

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Заметки по базовому реакту
+## Свойства (props)
+1. способ передачи props `function whoAmI? () {`
+`return {props.name} }`
+`whoAmI(Ivan)`
+2. Компонент реакта не может перерендерить новые значения props. 
+3. props могут быть любые типы данных.
+4. props мб и function and arrow function
+5. 
--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -4,10 +4,9 @@ import './employees-list.css'
 const EmployeesList = () => {
     return (
         <ul className='app-list list-group'>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            <EmployeesListItem name='John' surname = 'Smith' salary = {800}/>
+            <EmployeesListItem name='Alex' surname = 'Smith' salary = {100}/>
+            <EmployeesListItem name='Wi' surname = 'Smith' salary = {1200}/>
         </ul>
     )
 }
